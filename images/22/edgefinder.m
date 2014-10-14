bcabb975d41ec56d52bef6e326700a4b2cc96145
pfz3 <pfz3@gatech.edu>
2014-10-14 17:53:00 -0400

    updated edgefinding code

--- images/22/edgefinder.m
+++ images/22/edgefinder.m
@@ -8,160 +8,86 @@ function out = edgefinder()
 
 close all;
 
-%% look for stitched together image, otherwise create the stiched image
-
-curdir = 'J:\Users\Patxi\Documents\GitHub\MatInfTeam4.github.io\images\22';
-l = dir(curdir);
-
-flag =  find( cellfun(@(x)isequal(x,'stiched.png'),{l.name}) );
-
-if isempty(flag)
-    stichimages('J:\Users\Patxi\Dropbox\ME8333\22_S1');
-end
-
-% run a simple loop for now to go through all images and inspect
-
-
-file = ['J:\Users\Patxi\Dropbox\ME8333\22_S1\22\multifocus.tif'];
+for i = 1:1
+    
+%file = ['J:\Users\Patxi\Dropbox\ME8333\22_S1\',num2str(i,'%1.0f'),'\multifocus.tif'];
+file = ['J:\Users\Patxi\Dropbox\ME8333\22_S1\exposuretrial\z_0009.tif'];
+% file = ['J:\Users\Patxi\SkyDrive\MELKOTE\Experimental\CP-Ti\Pics\22\CpTi-22-20X-1.tif'];
 
 I = imread(file);
-
-% defines "fraction" of image to look at - useful for zooming in
-% on small patches [1e-5 to 1] represents the entire image
-start = 0.25;
-final = 0.75;
+start = 0.00001;
+final = 1.0;
 I2 = I(ceil(start*size(I,1)):final*size(I,1),ceil(size(I,2)*start):final*size(I,2),:);
 
-G = rgb2gray(I2);
+scale = 0.2;
+G1 = rgb2gray(I2);
+G = imresize(G1,scale,'bilinear');
 
-%% canny filter
-filter1 = 'canny';
-thresh1 = [0.45];
-
-BW1 = edge(G,filter1,thresh1);
 
 y = getmondim(1);
 h=figure('position',y);
 
-% plot original and the canny filters
-subplot(2,2,1);imshow(G); axis on;
-subplot(2,2,3); imshow(BW1); axis on; title([filter1,' filter, threshold = ',num2str(thresh1,'%1.2f')]);
+%% 1,1
+subplot(2,3,1);imshow(G); axis on;
+title(['Grayscale Image, scaling = ',num2str(scale,'%1.2f')]);
 
-%% multi/single thresh no filter
+%% 2,1
 
-[levels metric] = multithresh(G,2); % get two levels
-levels = double(levels)/256;        % normalize
-
-BW1 = im2bw(G,levels(1));           % get pixels above first threshold
-BW2 = 1-im2bw(G,levels(2));         % get pixels below first threshold
-BW = BW1 & BW2; BW = 1- BW;         % get intersection and inverse
+% [levels metric] = multithresh(G,2);
+% levels = double(levels)/256;
+[level metric] = graythresh(G);
+% BW1 = im2bw(G,levels(1));
+% BW2 = 1-im2bw(G,levels(2));
+% BW = BW1 & BW2; BW = 1- BW;
+BW = im2bw(G,level);
 figure(h);
 
-
-% plot the original image and overlay the thresholded image on top w gr
 green = cat(3,zeros(size(G)),ones(size(G)),zeros(size(G)));
-subplot(2,2,4); imshow(G);
-subplot(2,2,4); hold on; gh = imshow(green);
-title('multi-threshold');
+subplot(2,3,4); imshow(G);
+subplot(2,3,4); hold on; gh = imshow(green);
+title('Threshold');
 set(gh,'AlphaData',imcomplement(BW)); axis on;
 
+%% 2,2
+filter1 = 'canny';
+thresh1 = [];
 
+BW1 = edge(G,filter1,thresh1);
+
+subplot(2,3,5); imshow(BW1); axis on; title([filter1,' filter, threshold = ',num2str(thresh1,'%1.2f')]);
+
+%% 1,3
+[Gx, Gy] = imgradientxy(G);
+[Gmag,Gdir] = imgradient(Gx,Gy);
+
+subplot(2,3,3); imshow(Gdir); axis on; title('Gradient Dir');
+
+%% 2,3
+subplot(2,3,6); imshow(G1);
+
+green = cat(3,zeros(size(G1)),ones(size(G1)),zeros(size(G1)));
+subplot(2,3,6); hold on; gh = imshow(green);
+title('multi-threshold');
+set(gh,'AlphaData',imresize(BW1,[size(G1,1) size(G1,2)])); axis on;
+
+axis on; title('Canny + unscaled image');
 
 %% histogram
 [counts, x]=imhist(G);
-counts = counts*(1/sum(counts));    % normalize to get the ~pdf
+counts = counts*(1/sum(counts));
 
 figure(h);
-subplot(2,2,2); 
+subplot(2,3,2); 
 plot(x,counts); ys = get(gca,'Ylim'); hold on;
+
+try
 plot([levels(1) levels(1)]*256,ys,'r');
 plot([levels(2) levels(2)]*256,ys,'r');
+catch
+plot([level level]*256,ys,'r');
+end
 ylim(ys);title(['m1 = ', num2str(metric,'%1.2f')]);
 axis on; grid on;
 set(gcf,'color','w');
 saveas(h,num2str(i,'%1.0d'),'png');
-
-function stichimages(path)
-
-% goto directory with images
-l = dir(path);
-
-% find all the directories which contain the ii-th image
-pics =  length(find( cellfun(@(x)isequal(x,1),{l.isdir}) ))-2;
-
-% loop over each image and stich together
-% assumes that image order goes from 1 to ii
-img1 = (rgb2gray(imread([path,'\',num2str(6,'%1.0d'),'\multifocus.tif'])));
-img1 = img1 - (sum(sum(img1))/(size(img1,1)*size(img1,2)));
-for j = 7:pics
-    img2 = (rgb2gray(imread([path,'\',num2str(j,'%1.0d'),'\multifocus.tif'])));
-    img2 = img2 - (sum(sum(img2))/(size(img2,1)*size(img2,2)));
-    
-%     F1 = fftshift(fft2(img1));
-%     F2 = fftshift(fft2(img2));
-    F1 = (fft((img1)));
-    F2 = (fft((img2)));
-    F2 = conj(F2);
-    prod = F1.*F2;
-    Q = prod;
-    
-    Q = ifft(Q);
-    Q = Q-min(min(Q));
-    Q = Q/max(max(Q));
-    [X, Y] = find(Q == max(max(Q)));
-    Qs = sum(Q)/size(Q,1);
-    [val index] = max(Qs);
-    
-    
-    [r1 c1 d1] = size(img1);
-    [r2 c2 d2] = size(img2);
-
-    X = index-1;
-    Y = 0;
-
-    if img1(1,1)==img2(size(img1,2)-X,size(img1,1)-Y)
-        img = zeros(r2-X+r1,c2-Y+c1,3);
-        img=cast(img,'uint8');
-        img(1:r2,1:c2,:)=img2;
-        img(r2-X:r2-X+r1-1,c2-Y:c2-Y+c1-1,:)=img1;
-    else
-        img = zeros(r1,c2 + c1 - X,1);
-        img=cast(img,'uint8');
-        img(1:r1,1:c1,:)=img1;
-        img(1:r1,c1+1:c1+c2-X,:)=img2(:,X+1:c2,:);
-    end
-
-    y = getmondim(1);    
-    h=figure('position',y);
-    
-    
-    img=cast(img,'uint8');
-    subplot(4,2,[5,7]); imshow(img); axis on; freezeColors;
-    
-
-
-    subplot(4,2,[1,3]); imshow(img1); axis on; freezeColors;
-    s = subplot(4,2,[2,4]); imshow(img2); hold on; axis on; freezeColors;
-    pos2 = get(s,'Position');
-    
-    
-    s = subplot(4,2,6); contourf((Q)); colormap('jet');colorbar; freezeColors;
-    pos3 = get(s,'Position');
-    set(s,'Position',[pos2(1) pos3(2) pos2(3) pos3(4)]);
-    f = subplot(4,2,8); 
-    posf = get(f,'Position');
-    
-    %set(f,'Position',[posf(1) posf(2) pos3(3) posf(4)]);
-    plot(f,1:size(Q,2),Qs,'k'); hold on;
-    plot(f,[index index],[0 1],'r'); 
-    ylim(get(f,'ylim')); xlim(get(s,'xlim'));
-    
-    s=subplot(4,2,[2,4]); hold on;
-    plot(s,[index index],[1 size(img2,1)],'r');
-    
-
-    5;
 end
-
-
-
