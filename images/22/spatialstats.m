79f55e466fa10fc9f4851b8aecc5dd4b25d58dee
pfz3 <pfz3@gatech.edu>
2014-11-17 14:23:53 -0500

    chord length work

--- images/22/spatialstats.m
+++ images/22/spatialstats.m
@@ -2,20 +2,23 @@ function spatialstats()
 
 addpath('J:\Users\Patxi\Documents\GitHub\SpatialStatisticsFFT');
 
-filename{1} = 'J:\Users\Patxi\Dropbox\ME8333\24\MultiFocus.tif';
-filename{2} = 'J:\Users\Patxi\Dropbox\ME8333\24\MultiFocus1.tif';
-filename{3} = 'J:\Users\Patxi\Dropbox\ME8333\24\MultiFocus2.tif';
+filename{1} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic1\1.tif';
+filename{2} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic2\2.tif';
+filename{3} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic3\3.tif';
+filename{4} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic4\4.tif';
+filename{5} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic5\5.tif';
+filename{6} = 'J:\Users\Patxi\Dropbox\ME8333\24\Pic6\6.tif';
 % filename = 'J:\Users\Patxi\Dropbox\ME8333\22_S1\exposuretrial\multifocus.tif';
 
 for i = 1:length(filename)
-    [BW,IMG] = edgefinder(filename{i},0,0.25);
+    [BW,IMG] = edgefinder(filename{i},0,1.0);
     y = getmondim(1);
     h=figure('position',y);
     S = [1,2]
 
 
     [T xx] = SpatialStatsFFT(imcomplement(BW),[],'display',0);
-    T = log(T);
+    T = (T);
     T = flipdim(T,2);
     subplot(S(1),S(2),2); 
 
